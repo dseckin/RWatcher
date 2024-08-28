c5ff46589f5f0068d670a63fe13f3df634e2b1f3
Denizhan Seckin (hztnr) <denizhanseckin@gmail.com>
2024-08-28 14:55:53 +0000

    Watcher fully work with args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,87 @@
+use std::process::{Command, Stdio};
+use std::thread::sleep;
+use std::time::Duration;
+use std::env;
+
 fn main() {
-    println!("RWatcher!");
+    let args: Vec<String> = env::args().collect();
+    let command_string = match args.iter().find(|&x| x.starts_with("--cmd=")) {
+        Some(arg) => arg.trim_start_matches("--cmd=").to_string(),
+        None => {            
+            eprintln!("Error: --cmd parameter is missing.");
+            std::process::exit(-1);
+        },
+    };
+    let arg_string = match args.iter().find(|&x| x.starts_with("--arg=")) {
+        Some(arg) => arg.trim_start_matches("--arg=").to_string(),
+        None => "".to_string(),
+    };
+
+    let seconds_to_check = match args.iter().find(|&x| x.starts_with("--sec=")) {
+        Some(arg) => arg.trim_start_matches("--sec=").to_string().parse::<i32>().unwrap(),
+        None => 5,
+    };
+    let seconds_to_kill = match args.iter().find(|&x| x.starts_with("--kill_sec=")) {
+        Some(arg) => arg.trim_start_matches("--kill_sec=").to_string().parse::<i32>().unwrap(),
+        None => -1,
+    };
+    
+    let mut sleep_time_count = 0;
+    let mut collector_process = Command::new(command_string.clone())
+        .arg(arg_string.clone())
+        .stdin(Stdio::null())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .expect("failed to start collector program");
+
+    loop {
+        sleep(Duration::from_secs(seconds_to_check.try_into().unwrap()));
+        sleep_time_count += 1;
+        
+        match collector_process.try_wait() {
+            Ok(Some(_)) => {
+                // Program stopped running
+                // Restart the program
+                collector_process = Command::new(command_string.clone())
+                    .arg(arg_string.clone())
+                    .stdin(Stdio::null())
+                    .stdout(Stdio::piped())
+                    .stderr(Stdio::piped())
+                    .spawn()
+                    .expect("failed to start collector program");
+                
+                sleep_time_count = 0;
+            }
+            Ok(None) => {
+                // Program still running                
+                // Kill the collector program forcibly
+                if seconds_to_kill == -1 {
+                    continue;
+                }
+
+                if sleep_time_count >= seconds_to_kill {
+                    let _ = collector_process.kill();
+                    // Çıkış durumunu bekleyerek zombie durumunu engelle
+                    let _ = collector_process.wait();
+
+                    collector_process = Command::new(command_string.clone())
+                        .arg(arg_string.clone())
+                        .stdin(Stdio::null())
+                        .stdout(Stdio::piped())
+                        .stderr(Stdio::piped())
+                        .spawn()
+                        .expect("failed to start collector program");
+
+                    
+                    sleep_time_count = 0;
+                }
+            }
+            Err(e) => {
+                // Error occurred                
+                eprintln!("{}", format!("Error checking program status: {}", e));
+            }
+        }//match
+    }
+
 }
